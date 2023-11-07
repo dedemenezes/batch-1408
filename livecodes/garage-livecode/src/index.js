b7eccd657a82f5e6517aeaaa3ad386d469582dc0
Andre Menezes <djdedemenezes@gmail.com>
2023-11-07 18:25:15 -0300

    add refactoring

--- livecodes/garage-livecode/src/index.js
+++ livecodes/garage-livecode/src/index.js
@@ -1,51 +1,43 @@
-// console.log("Connected")
-// FETCH ALL CARS
-// 1 . retrieve all cars from the API (fetch)
+// It's a good practice to have al your selector at the top
+// because this way
+// THEY ARE AVAILABLE IN EVERY SINGLE FUNCTION!!!
+const carsList = document.querySelector('.cars-list')
+const form = document.querySelector('.car-form')
+const [brand, model, plate, owner] = document.querySelectorAll('.form-control')
+
+
+// This function has a car Javascript Object as a parameter
+// And it build the car card and insert it into the HTML
+const insertInList = (carObject) => {
+  const carHTML = `<div class="car">
+          <div class="car-image">
+            <img src="http://loremflickr.com/280/280/${carObject.brand}${carObject.model}" />
+          </div>
+          <div class="car-info">
+            <h4>${carObject.brand} ${carObject.model}</h4>
+            <p><strong>Owner:</strong> ${carObject.owner}</p>
+            <p><strong>Plate:</strong> ${carObject.plate}</p>
+          </div>
+        </div>`
+  carsList.insertAdjacentHTML('afterbegin', carHTML)
+}
+
+// This is our first function that retrieves all cars
+// And at the end cal the insertInList to add to the HTML
 const fetchAllCars = () => {
   fetch("https://wagon-garage-api.herokuapp.com/cool-garage/cars")
     .then(response => response.json())
     .then((data) => {
-      // console.log(data)
-      const carsList = document.querySelector('.cars-list')
       carsList.innerHTML = ''
-      // 2.0 we need to iterate over all cars
-      data.forEach((car) => {
-        console.log(car)
-        // car is something like => { id: 16205, brand: 'Jeep', model: 'Gladiator', plate: '123CP56', owner: 'Camila', … }
-        // 2. Display each one to the user (insert into the HTML)
-
-        const carHTML = `<div class="car">
-          <div class="car-image">
-            <img src="http://loremflickr.com/280/280/${car.brand}${car.model}" />
-          </div>
-          <div class="car-info">
-            <h4>${car.brand} ${car.model}</h4>
-            <p><strong>Owner:</strong> ${car.owner}</p>
-            <p><strong>Plate:</strong> ${car.plate}</p>
-          </div>
-        </div>`
-        carsList.insertAdjacentHTML('afterbegin', carHTML)
-      })
+      data.forEach(car => insertInList(car))
     })
 }
 
-fetchAllCars() // FIRST LOAD!
-// ADD A NEW CAR
-// 1. Select the elements
-const form = document.querySelector('.car-form')
-// 2. bind an event listener
-form.addEventListener('submit', (event) => {
+// This was our second Challenge
+// We submit the form using AJAX
+// At the end we call the fetchAllCars to update the HTML accordingly
+const createCar = (event) => {
   event.preventDefault()
-  // 3. PRvent the page to refresh
-  // console.log('TESTINGGGGG')
-  // 4. retrieve every input value
-  // DECONSTRUCT
-  const [brand, model, plate, owner] = document.querySelectorAll('.form-control')
-  // console.log(brand.value)
-  // console.log(model)
-  // console.log(plate)
-  // console.log(owner)
-  // 5. send our form to the API (fetch - POST)
   fetch("https://wagon-garage-api.herokuapp.com/cool-garage/cars", {
     method: 'POST',
     headers: { "Content-Type": "application/json" },
@@ -56,11 +48,13 @@ form.addEventListener('submit', (event) => {
       plate: plate.value
     })
   })
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data)
-    // FETCH ALL CARS
-    fetchAllCars()
-  })
-})
-// 6. display the new car
+    .then(response => response.json())
+    .then(() => fetchAllCars())
+}
+
+// On the second challenge, everything will start when the user submit the form
+// We listen to the event and when it's fired
+// the callback createCar will run.
+form.addEventListener('submit', createCar)
+
+fetchAllCars() // FIRST LOAD!
